5158b2178b868de0eba82cb7039679d2a3a890d9
Akhil <39794209+kakhilR@users.noreply.github.com>
2026-07-22 07:49:53 +0000

    template

---
+++ springprojects/src/test/java/com/example/springprojects/SpringprojectsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springprojects;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringprojectsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
